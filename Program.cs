90426696dda17bca377c4095a4c7c23d6d28dbfc
phanxuanquang <84692657+phanxuanquang@users.noreply.github.com>
2021-09-19 21:08:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student();
+            a.infoImport();
+            a.infoExport();
+            Console.ReadKey();
+        }
+    }
+    
+}
