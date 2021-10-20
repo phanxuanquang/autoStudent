707461dfa6fb1e414504bf9c356267f6fa5d31d0
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2021-10-20 19:35:15 +0700

    Sửa UI

--- App/View/BaseSectorView.Designer.cs
+++ App/View/BaseSectorView.Designer.cs
@@ -30,33 +30,44 @@ namespace App.View
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(548, 317);
+            this.button1.Location = new System.Drawing.Point(585, 383);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(74, 48);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Cài đặt";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(695, 359);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(130, 86);
+            this.checkedListBox1.Location = new System.Drawing.Point(124, 85);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(438, 244);
+            this.checkedListBox1.Size = new System.Drawing.Size(117, 19);
             this.checkedListBox1.TabIndex = 3;
             // 
-            // BaseView
+            // BaseSectorView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
-            this.Name = "BaseView";
+            this.Name = "BaseSectorView";
             this.Size = new System.Drawing.Size(698, 457);
             this.ResumeLayout(false);
 
@@ -64,6 +75,7 @@ namespace App.View
 
         #endregion
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
