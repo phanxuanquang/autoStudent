978ddb5026c5f6e0db70c8b5fd3cde762d86e7d9
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2021-11-08 16:35:10 +0700

    Sửa tiếp

--- App/MainWindow.Designer.cs
+++ App/MainWindow.Designer.cs
@@ -32,7 +32,7 @@ namespace App
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.menuPanel = new System.Windows.Forms.Panel();
             this.MultimediaTab = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.bunifuFlatButton4 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.GitHub_Button = new Bunifu.Framework.UI.BunifuFlatButton();
             this.TechnicalityTab = new Bunifu.Framework.UI.BunifuFlatButton();
             this.ITTab = new Bunifu.Framework.UI.BunifuFlatButton();
             this.menuButton = new Bunifu.Framework.UI.BunifuImageButton();
@@ -52,7 +52,7 @@ namespace App
             | System.Windows.Forms.AnchorStyles.Left)));
             this.menuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
             this.menuPanel.Controls.Add(this.MultimediaTab);
-            this.menuPanel.Controls.Add(this.bunifuFlatButton4);
+            this.menuPanel.Controls.Add(this.GitHub_Button);
             this.menuPanel.Controls.Add(this.TechnicalityTab);
             this.menuPanel.Controls.Add(this.ITTab);
             this.menuPanel.Controls.Add(this.menuButton);
@@ -100,42 +100,43 @@ namespace App
             this.MultimediaTab.TextFont = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.MultimediaTab.Click += new System.EventHandler(this.MultimediaTab_Click);
             // 
-            // bunifuFlatButton4
+            // GitHub_Button
             // 
-            this.bunifuFlatButton4.Active = false;
-            this.bunifuFlatButton4.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
-            this.bunifuFlatButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
-            this.bunifuFlatButton4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bunifuFlatButton4.BorderRadius = 0;
-            this.bunifuFlatButton4.ButtonText = "Github";
-            this.bunifuFlatButton4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bunifuFlatButton4.DisabledColor = System.Drawing.Color.Gray;
-            this.bunifuFlatButton4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.bunifuFlatButton4.Iconcolor = System.Drawing.Color.Transparent;
-            this.bunifuFlatButton4.Iconimage = null;
-            this.bunifuFlatButton4.Iconimage_right = null;
-            this.bunifuFlatButton4.Iconimage_right_Selected = null;
-            this.bunifuFlatButton4.Iconimage_Selected = null;
-            this.bunifuFlatButton4.IconMarginLeft = 0;
-            this.bunifuFlatButton4.IconMarginRight = 0;
-            this.bunifuFlatButton4.IconRightVisible = true;
-            this.bunifuFlatButton4.IconRightZoom = 0D;
-            this.bunifuFlatButton4.IconVisible = true;
-            this.bunifuFlatButton4.IconZoom = 90D;
-            this.bunifuFlatButton4.IsTab = false;
-            this.bunifuFlatButton4.Location = new System.Drawing.Point(0, 546);
-            this.bunifuFlatButton4.Margin = new System.Windows.Forms.Padding(0);
-            this.bunifuFlatButton4.Name = "bunifuFlatButton4";
-            this.bunifuFlatButton4.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
-            this.bunifuFlatButton4.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
-            this.bunifuFlatButton4.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
-            this.bunifuFlatButton4.selected = false;
-            this.bunifuFlatButton4.Size = new System.Drawing.Size(135, 44);
-            this.bunifuFlatButton4.TabIndex = 7;
-            this.bunifuFlatButton4.Text = "Github";
-            this.bunifuFlatButton4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.bunifuFlatButton4.Textcolor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
-            this.bunifuFlatButton4.TextFont = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GitHub_Button.Active = false;
+            this.GitHub_Button.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
+            this.GitHub_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
+            this.GitHub_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GitHub_Button.BorderRadius = 0;
+            this.GitHub_Button.ButtonText = "Github";
+            this.GitHub_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.GitHub_Button.DisabledColor = System.Drawing.Color.Gray;
+            this.GitHub_Button.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GitHub_Button.Iconcolor = System.Drawing.Color.Transparent;
+            this.GitHub_Button.Iconimage = null;
+            this.GitHub_Button.Iconimage_right = null;
+            this.GitHub_Button.Iconimage_right_Selected = null;
+            this.GitHub_Button.Iconimage_Selected = null;
+            this.GitHub_Button.IconMarginLeft = 0;
+            this.GitHub_Button.IconMarginRight = 0;
+            this.GitHub_Button.IconRightVisible = true;
+            this.GitHub_Button.IconRightZoom = 0D;
+            this.GitHub_Button.IconVisible = true;
+            this.GitHub_Button.IconZoom = 90D;
+            this.GitHub_Button.IsTab = false;
+            this.GitHub_Button.Location = new System.Drawing.Point(0, 546);
+            this.GitHub_Button.Margin = new System.Windows.Forms.Padding(0);
+            this.GitHub_Button.Name = "GitHub_Button";
+            this.GitHub_Button.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(251)))), ((int)(((byte)(217)))));
+            this.GitHub_Button.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.GitHub_Button.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
+            this.GitHub_Button.selected = false;
+            this.GitHub_Button.Size = new System.Drawing.Size(135, 44);
+            this.GitHub_Button.TabIndex = 7;
+            this.GitHub_Button.Text = "Github";
+            this.GitHub_Button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.GitHub_Button.Textcolor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(29)))), ((int)(((byte)(67)))));
+            this.GitHub_Button.TextFont = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GitHub_Button.Click += new System.EventHandler(this.GitHub_Button_Click);
             // 
             // TechnicalityTab
             // 
@@ -309,7 +310,7 @@ namespace App
 
         #endregion
         private System.Windows.Forms.Panel menuPanel;
-        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton4;
+        private Bunifu.Framework.UI.BunifuFlatButton GitHub_Button;
         private Bunifu.Framework.UI.BunifuFlatButton TechnicalityTab;
         private Bunifu.Framework.UI.BunifuFlatButton ITTab;
         private Bunifu.Framework.UI.BunifuImageButton menuButton;
