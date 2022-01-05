bdb9431f85ee4ee5b937c382e7d5d66d274c2df2
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2022-01-05 17:32:37 +0700

    test

--- App/Program.cs
+++ App/Program.cs
@@ -15,7 +15,7 @@ namespace App
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll")]  
         public static extern bool SetForegroundWindow(IntPtr hWnd);
         [STAThread]
          
